a5273bdb8321d16faa67d78115f19b55f9d90f09
hypeboard <37452765+hypeboard@users.noreply.github.com>
2019-07-03 08:33:48 -0500

    updated

--- src/Rpc/CoreRpcServerCommandsDefinitions.h
+++ src/Rpc/CoreRpcServerCommandsDefinitions.h
@@ -38,6 +38,16 @@ struct STATUS_STRUCT {
   }
 };
 
+struct STATUS_STRUCT_EX {
+  std::string status;
+  std::string data;
+
+   void serialize(ISerializer &s) {
+    KV_MEMBER(status)
+    KV_MEMBER(data)
+  }
+};
+
 struct COMMAND_RPC_GET_HEIGHT {
   typedef EMPTY_STRUCT request;
 
@@ -444,7 +454,7 @@ struct COMMAND_RPC_GET_CURRENCY_ID {
 
 struct COMMAND_RPC_SUBMITBLOCK {
   typedef std::vector<std::string> request;
-  typedef STATUS_STRUCT response;
+  typedef STATUS_STRUCT_EX response;
 };
 
 struct block_header_response {
